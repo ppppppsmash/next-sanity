c9628b84cae64befb63813cea05bf7ef3e9a727a
ppppppsmash <pppppp.work@gmail.com>
2023-05-30 00:21:02 +0900

    add: sanity管理画面にカスタマイズnavコンポーネント

---
+++ components/StudioNavbar.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
+
+export function StudioNavbar(props: any) {
+  return (
+    <div>
+      <div className='flex items-center justify-between p-5'>
+        <Link href='/' className='text-[#93E6B4] flex items-center'>
+          <ArrowUturnLeftIcon className='h-6 w-6 text-[#93E6B4] mr-2' />
+          ページに戻る
+        </Link>
+
+        <div className='hidden md:flex p-5 rounded-lg justify-center border-2 border-[#93E6B4]'>
+          <h1 className='font-bold text-white'>
+            テキストテキストテキスト
+          </h1>
+          <Link
+            className='text-#93E6B4 font-bold ml-2'
+            href='https://www.xxx'
+          >
+            xxx
+          </Link>
+        </div>
+      </div>
+      {/* sanityのデフォルト検索欄 */}
+      <>{props.renderDefault(props)}</>
+    </div>
+  )
+}
--- sanity.config.ts
+++ sanity.config.ts
@@ -8,7 +8,9 @@ import {deskTool} from 'sanity/desk'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schema'
+import {schema} from '@/sanity/schema'
+import { myTheme } from '@/theme'
+import { StudioNavbar } from '@/components/StudioNavbar'
 
 export default defineConfig({
   basePath: '/studio',
@@ -24,4 +26,11 @@ export default defineConfig({
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
   ],
+  studio: {
+    components: {
+      //logo: Logo,
+      navbar: StudioNavbar
+    }
+  },
+  theme: myTheme
 })
