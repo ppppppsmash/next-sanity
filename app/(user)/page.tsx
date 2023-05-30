d8cde6ec5e05f5789f99f03fe1289b8577beb214
ppppppsmash <pppppp.work@gmail.com>
2023-05-31 01:52:12 +0900

    add: プレビュー機能(previewDataエラーing...)

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,8 +1,27 @@
-import Image from 'next/image'
+import { previewData } from 'next/headers'
+import { groq } from 'next-sanity'
+
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`
+
 
 export default function Home() {
+  if(previewData()) {
+    return (
+      <div>
+        Preview mode
+      </div>
+    )
+  }
+
   return (
     <div>
+      <h1>Not in Preview mode</h1>
     </div>
   )
 }
---
+++ lib/sanity.preview.ts
@@ -0,0 +1,18 @@
+'use client'
+
+import { definePreview } from 'next-sanity/preview'
+import { projectId, dataset } from './sanity.client'
+
+function onPublicAccessOnly() {
+  throw new Error('ログインしないとプレビューできない')
+}
+
+if(!projectId || !dataset) {
+  throw new Error('sanity.jsonと.envを確認してください')
+}
+
+export const usePreview = definePreview({
+  projectId,
+  dataset,
+  onPublicAccessOnly,
+})
