c50c806bc04089e7c12c9cb6db46a14103a6fd91
ppppppsmash <pppppp.work@gmail.com>
2023-05-30 23:54:06 +0900

    fix: ファイル構成を調整

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl">Pei's Blog</h1>
+      <h1 className="text-4xl">バイのページ</h1>
       <p>コンテンツ</p>
     </div>
   )
