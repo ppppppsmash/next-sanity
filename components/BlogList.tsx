8108e69d88c1e1d32c1b82f6f623ffa6ea715a44
ppppppsmash <pppppp.work@gmail.com>
2023-06-01 23:58:28 +0900

    mod: 各slugへのアクセス

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -1,6 +1,7 @@
 import { urlFor } from "@/lib/urlFor"
 import { ArrowUpRightIcon } from "@heroicons/react/24/solid"
 import Image from "next/image"
+import ClientSideRoute from "@/components/ClientSideRoute"
 
 type Props = {
   posts: Post[]
@@ -13,53 +14,56 @@ export default function BlogList({ posts }: Props) {
 
       <div className='grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24'>
         {posts.map((post) => (
-          <div
-            className='group flex-col cursor-pointer'
+          <ClientSideRoute
+            route={`/post/${post.slug.current}`}
             key={post._id}>
-            <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105
-              transition-transform duration-200 ease-out'>
-              <Image
-                className='object-cover object-left lg:object-center'
-                src={urlFor(post.mainImage).url()}
-                alt={post.author.name}
-                fill
-              />
-              <div className='absolute bottom-0 w-full bg-opacity-20 bg-black
-                backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
-                <div>
-                  <p className='font-bold'>{post.title}</p>
-                  <p>
-                    {new Date(post._createdAt).toLocaleDateString
-                    ('ja-JP', {
-                      day: 'numeric',
-                      month: 'long',
-                      year: 'numeric'
-                    })
-                    }
-                  </p>
-                </div>
+            <div className='flex flex-col group cursor-pointer'>
+              <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105
+                transition-transform duration-200 ease-out'>
+                <Image
+                  className='object-cover object-left lg:object-center'
+                  src={urlFor(post.mainImage).url()}
+                  alt={post.author.name}
+                  fill
+                />
+                <div className='absolute bottom-0 w-full bg-opacity-20 bg-black
+                  backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                  <div>
+                    <p className='font-bold'>{post.title}</p>
+                    <p>
+                      {new Date(post._createdAt).toLocaleDateString
+                      ('ja-JP', {
+                        day: 'numeric',
+                        month: 'long',
+                        year: 'numeric'
+                      })
+                      }
+                    </p>
+                  </div>
 
-                <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
-                    {post.categories.map((category) => (
-                      <div className='bg-[#93E6B4] text-center text-black px-3
-                        py-1 rounded-full text-sm font-semibold'
-                      >
-                        <p>{category.title}</p>
-                      </div>
-                    ))}
+                  <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
+                      {post.categories.map((category) => (
+                        <div className='bg-[#93E6B4] text-center text-black px-3
+                          py-1 rounded-full text-sm font-semibold'
+                        >
+                          <p>{category.title}</p>
+                        </div>
+                      ))}
+                  </div>
                 </div>
+              
               </div>
-            </div>
-            <div className='mt-5 flex-1'>
-              <p className='underline text-lg font-bold'>{post.title}</p>
-              <p className='line-clamp-2 text-gray-500'>{post.description}</p>
-            </div>
+              <div className='mt-5 flex-1'>
+                <p className='underline text-lg font-bold'>{post.title}</p>
+                <p className='line-clamp-2 text-gray-500'>{post.description}</p>
+              </div>
 
-            <p className='mt-5 font-bold flex items-center group-hover:underline'>
-              もっと
-              <ArrowUpRightIcon className='ml-2 h-4 w-4' />
-            </p>
-          </div>
+              <p className='mt-5 font-bold flex items-center group-hover:underline'>
+                もっと
+                <ArrowUpRightIcon className='ml-2 h-4 w-4' />
+              </p>
+            </div>
+          </ClientSideRoute>
         ))}
       </div>
     </div>
---
+++ components/ClientSideRoute.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Link from 'next/link'
+
+export default function ClientSideRoute({
+  children, route
+}: {
+  children: React.ReactNode
+  route: string
+}) {
+  return (
+    <Link href={route}>
+      {children}
+    </Link>
+  )
+}
