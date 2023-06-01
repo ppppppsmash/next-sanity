6b59595615dd4a51435a1f880fdd7689b12f3825
ppppppsmash <pppppp.work@gmail.com>
2023-06-02 00:20:13 +0900

    add: 管理画面にプレビュー機能を追加

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,6 +12,7 @@ import {schema} from '@/sanity/schema'
 import { myTheme } from '@/theme'
 import { StudioNavbar } from '@/components/StudioNavbar'
 import { Logo } from '@/components/Logo'
+import { getDefaultDocumentNode } from '@/structure'
 
 export default defineConfig({
   basePath: '/studio',
@@ -22,7 +23,9 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schema' folder
   schema,
   plugins: [
-    deskTool(),
+    deskTool({
+      defaultDocumentNode: getDefaultDocumentNode,
+    }),
     // Vision is a tool that lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
