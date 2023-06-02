37e0559922fe6fb7af0f42a0ec2817b303ad106f
ppppppsmash <pppppp.work@gmail.com>
2023-06-02 23:35:12 +0900

    fix: 検索ページを一旦取り消し

--- app/search/[searchTerm]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default async function Loading() {
-  return (
-    <div>
-      Loading...🌀
-    </div>
-  )
-}
--- app/search/head.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Head() {
-  return (
-    <>
-      <title>検索ページ</title>
-    </>
-  )
-}
