edabb39068884a76af55333ee8de9c91afe29d14
ppppppsmash <pppppp.work@gmail.com>
2023-06-02 23:54:34 +0900

    mod: スタイリング

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -34,15 +34,50 @@ export default async function Post({ params: { slug }}: Props) {
             />
           </div>
 
-          <section>
-            <div>
+          <section className='p-5 bg-[#93E6B4] w-full'>
+            <div className='flex flex-col md:flex-row justify-between gap-y-5'>
               <div>
-                <h1>
-
+                <h1 className='text-4xl font-extrabold'>
+                  {post.title}
                 </h1>
                 <p>
-                  
+                  {new Date(post._createdAt).toLocaleDateString('ja-JP', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric'
+                  })}
                 </p>
+              </div>
+              <div className='flex items-center space-x-2'>
+                <Image
+                  className='rounded-full'
+                  src={urlFor(post.author.image).url()}
+                  alt={post.author.name}
+                  width={40}
+                  height={40}
+                />
+
+                <div className='w-64'>
+                  <h3>{post.author.name}</h3>
+                  <div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h2 className='italic pt-10'>{post.description}</h2>
+              <div className='flex items-center justify-end mt-auto space-x-2'>
+                {post.categories.map((category) => (
+                  <p
+                    key={category._id}
+                    className='bg-gray-800 text-white px-3 py-1 rounded-full
+                      text-sm font-semibold mt-4'
+                  >
+                    {category.title}
+                  </p>
+                ))}
               </div>
             </div>
           </section>
